b6474c2601dcce4610fc6dc8868f5f2fec4d275a
Marcin Swornowski <swor71@gmail.com>
2018-05-31 13:09:44 +0200

    testing

--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -5,29 +5,34 @@ import _ from 'lodash';
 import Header from './Header'
 
 const WeatherInfo = (props) => {
+    
+    let date = props.list.map(item => item.dt_txt.split(' ').shift());
 
-    let listItems = props.list.map((item, key) => (
+    const chunked = _.chunk(props.list, 8);
 
-        <tr key={key}>
-            <td>{item.dt_txt}</td>
-            <td>
-                <img className={"pic"} src={`http://openweathermap.org/img/w/${item.weather[0].icon}.png`} alt="icon" />
-            </td>
-            <td>
-                {item.weather[0].description}
-            </td>
-            <td>
-                {_.round(item.main.temp, 1)} &#8451;
-            </td>
-        </tr>
-    ))
+    const sliced = _.slice(chunked, 0, 1);
+
+    console.log(sliced);
 
     return (
         <div className={'weather-info'}>
             <Header city={props.city} />
             <table className='table'>
                 <tbody>
-                    {listItems}
+                    {sliced.map((item, key) => (
+                        <tr key={key}>
+                            <td>{item[key].dt_txt}</td>
+                            <td>
+                                <img className={"pic"} src={`http://openweathermap.org/img/w/${item[key].weather[0].icon}.png`} alt="icon" />
+                            </td>
+                            <td>
+                                {item[key].weather[0].description}
+                            </td>
+                            <td>
+                                {_.round(item[key].main.temp, 1)} &#8451;
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
